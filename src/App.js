30dd3b5ea9476b3f63da4d68880599d4707912c2
unknown <equiposune@gmail.com>
2021-05-01 16:20:59 -0500

    Navegador creado

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import Navegador from './components/Navegador'
 
 function App() {
   return (
     <div className="container mt-5" >
       <h1>Tareas</h1>
       <hr/>
+
+      <Navegador />
     </div>
   )
 }
---
+++ src/components/Navegador.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Navegador() {
+    return (
+        <div>
+            Navegador
+        </div>
+    )
+}
+
+export default Navegador
